02fa290ec45212424552498fa3ff21cd034123cc
QtrinoIndia <78023189+QtrinoIndia@users.noreply.github.com>
2021-03-11 15:28:38 +0530

    head viewport

--- assets/head.js
+++ assets/head.js
@@ -1,6 +1,6 @@
 
 document.getElementById("head").innerHTML =
-+'<meta name="viewport" content="width=device-width, initial-scale=1.0">'
+ '<meta name="viewport" content="width=device-width, initial-scale=1.0">'
 +'<meta charset="utf-8">'
 +'<link rel="shortcut icon" href="https://qtrinoindia.github.io/style/img/logo/qtrino.ico" type="image/x-icon">'
 +'<link rel="stylesheet" href="https://qtrinoindia.github.io/style/css/nav-body-foot.css">'
